062592bd5508e4d914de150ea229aff18054f74b
Andres Peguero <apeguero24@gmail.com>
2018-01-09 16:34:28 -0500

    Added presenter plus network request to list_names

---
+++ NYTBestseller/UI/Categories/presenters/CategoriesPresenter.swift
@@ -0,0 +1,24 @@
+//
+//  CategoriesPresenter.swift
+//  NYTBestseller
+//
+//  Created by Andres Peguero on 1/9/18.
+//  Copyright © 2018 Andres. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class CategoriesPresenter {
+    
+    func requestBookCategories() {
+        NYTNetwork.default.request(target: .listNames, success: { (data) in
+            let json = JSON(data as Any)
+            print(json["results"].arrayValue)
+        }, error: { (error) in
+            print(error)
+        }) { (failure) in
+            print(failure)
+        }
+    }
+}
