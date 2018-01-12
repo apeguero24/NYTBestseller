a63f5f1944d65011963f0f2a507b3257d48d403a
Andres Peguero <apeguero24@gmail.com>
2018-01-12 11:12:58 -0500

    detail book view with book cover

--- NYTBestseller/Models/Book.swift
+++ NYTBestseller/Models/Book.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 class Book: NSObject, NSCoding {
     var title: String
--- NYTBestseller/UI/Book Detail/BookDetailViewController.swift
+++ NYTBestseller/UI/Book Detail/BookDetailViewController.swift
@@ -14,6 +14,7 @@ class BookDetailViewController: UIViewController {
     @IBOutlet weak var coverImageView: UIImageView!
     @IBOutlet weak var amazonLinkLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
     
     let presenter = BookDetailPresenter()
     
@@ -23,7 +24,14 @@ class BookDetailViewController: UIViewController {
         if let book = presenter.book {
             titleLabel.text = book.title
             amazonLinkLabel.text = book.amazonLink
+            authorLabel.text = book.author
             descriptionLabel.text = book.bookDescription
+            coverImageView.kf.setImage(with: book.coverLink, placeholder: nil, options: nil,
+                                       progressBlock: nil, completionHandler: { (image, error, _, _) in
+                if image == nil {
+                    self.coverImageView.image = #imageLiteral(resourceName: "no_photo_avail")
+                }
+            })
         }
     }
 
